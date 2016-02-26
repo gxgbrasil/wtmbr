a7865f45caffbb525ebeece5e7ec9918017b6a8a
Ana Soares <anne.cr.soares@gmail.com>
2016-02-26 13:05:47 -0300

    Adiciona novos pontos no mapa

--- maps.js
+++ maps.js
@@ -16,7 +16,12 @@ function initialize() {
       ['WTM Rio de Janeiro', -22.90685, -43.17290],
       ['WTM Manaus', -3.11903,	-60.02173],
       ['WTM Maceió', -9.64985,	-35.70895],
-      ['WTM São Luís', -2.53911,	-44.28290]
+      ['WTM São Luís', -2.53911,	-44.28290],
+      ['WTM Curitiba', -25.4244287,	-49.2653819],
+      ['WTM Juiz de Fora', -21.7624237,	-43.3433999],
+      ['WTM Goiânia', -16.6868824,	-49.2647885],
+      ['WTM João Pessoa', -7.1194958,	-34.8448668],
+      ['WTM São João da Boa Vista', -21.9712237,	-46.7947225]
   ];
 
   var infoWindowContent = [
@@ -51,6 +56,26 @@ function initialize() {
       ['<div class="info_content">' +
       '<strong>WTM São Luís</strong><br>' +
       '<a href="https://www.facebook.com/GDGSaoLuis">Facebook</a>' +
+      '</div>'],
+      ['<div class="info_content">' +
+      '<strong>WTM Curitiba</strong><br>' +
+      '<a href="https://www.facebook.com/Women-Techmakers-Curitiba-886440344765453/">Facebook</a>' +
+      '</div>'],
+      ['<div class="info_content">' +
+      '<strong>WTM Juiz de Fora</strong><br>' +
+      '<a href="http://www.womentechmakersjf.org/">Site</a>' +
+      '</div>'],
+      ['<div class="info_content">' +
+      '<strong>WTM Goiânia</strong><br>' +
+      '<a href="http://www.gdggoiania.org/">Site do GDG</a>' +
+      '</div>'],
+      ['<div class="info_content">' +
+      '<strong>WTM João Pessoa</strong><br>' +
+      '<a href="https://www.facebook.com/wtmjp/">Facebook</a>' +
+      '</div>'],
+      ['<div class="info_content">' +
+      '<strong>WTM São João da Boa Vista</strong><br>' +
+      '<a href="https://www.facebook.com/gdgsaojoao/">Facebook do GDG</a>' +
       '</div>']
   ];
 
